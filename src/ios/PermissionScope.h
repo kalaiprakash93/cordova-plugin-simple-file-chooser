d081fd6f5a101d40c5d797e018f5b3de0c457f09
kalaiprakash93 <kalaiprakash93@gmail.com>
2022-02-02 13:46:15 +0530

    Create PermissionScope.h

---
+++ src/ios/PermissionScope.h
@@ -0,0 +1,8 @@
+#import <UIKit/UIKit.h>
+#import <Cordova/CDVPlugin.h>
+
+//! Project version number for modalperms.
+FOUNDATION_EXPORT double PermissionScopeVersionNumber;
+
+//! Project version string for modalperms.
+FOUNDATION_EXPORT const unsigned char PermissionScopeVersionString[];
