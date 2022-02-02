b3fe9b3981d126a33e6c4498bb33e0c502b4b413
kalaiprakash93 <kalaiprakash93@gmail.com>
2022-02-02 14:15:56 +0530

    Update and rename PermissionScope.h to Chooser.h

--- src/ios/Chooser.h
+++ src/ios/Chooser.h
@@ -2,7 +2,7 @@
 #import <Cordova/CDVPlugin.h>
 
 //! Project version number for modalperms.
-FOUNDATION_EXPORT double PermissionScopeVersionNumber;
+FOUNDATION_EXPORT double ChooserVersionNumber;
 
 //! Project version string for modalperms.
-FOUNDATION_EXPORT const unsigned char PermissionScopeVersionString[];
+FOUNDATION_EXPORT const unsigned char ChooserVersionString[];
