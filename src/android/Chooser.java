5bd8d474c8df74ca16f93f30bc5bcc0c1e6f5277
kalaiprakash93 <kalaiprakash93@gmail.com>
2022-02-27 10:57:47 +0530

    Update Chooser.java

--- src/android/Chooser.java
+++ src/android/Chooser.java
@@ -63,7 +63,7 @@ public class Chooser extends CordovaPlugin {
         intent.putExtra(Intent.EXTRA_MIME_TYPES, accept.split(","));
         intent.addCategory(Intent.CATEGORY_OPENABLE);
         intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
-        intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
+        intent.putExtra(Intent.EXTRA_LOCAL_ONLY, false);
 
         Intent chooser = Intent.createChooser(intent, "Select File");
         cordova.startActivityForResult(this, chooser, Chooser.PICK_FILE_REQUEST);
